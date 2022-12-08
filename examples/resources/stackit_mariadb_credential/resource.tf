8eef302ec8a524b047e776ba3da905fce08ceb45
Johann Friesen <johann.friesen@camao.one>
2022-12-08 15:39:34 +0100

    docs examples

---
+++ examples/resources/stackit_mariadb_credential/resource.tf
@@ -0,0 +1,9 @@
+resource "stackit_mariadb_instance" "example" {
+  name       = "example"
+  project_id = "example"
+}
+
+resource "stackit_mariadb_credential" "example" {
+  project_id = "example"
+  instance_id = stackit_mariadb_instance.example.id
+}
