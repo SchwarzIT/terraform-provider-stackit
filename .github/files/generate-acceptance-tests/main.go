33951ddfd470790d4e8845a85f1a2953b2d43a4a
Dean Oren <deangili.oren@mail.schwarz>
2023-01-20 18:08:47 +0100

    add resources

--- .github/files/generate-acceptance-tests/main.go
+++ .github/files/generate-acceptance-tests/main.go
@@ -77,8 +77,10 @@ func main() {
 	}
 	sData := string(data)
 
-	dsstr, deleteNeeds := printDataSourceOutcome(sortedGlobalKeysDS, dsk, sds, "ds")
+	dsstr, resNeeds := printDataSourceOutcome(sortedGlobalKeysDS, dsk, sds, "ds")
+	resstr, deleteNeeds := printResourceOutcome(sortedGlobalKeysRes, rk, sr, "res", resNeeds)
 	sData = strings.Replace(sData, "__data_sources__", dsstr, 1)
+	sData = strings.Replace(sData, "__resources__", resstr, 1)
 	sData = strings.Replace(sData, "__delete_needs__", deleteNeeds, 1)
 
 	err = ioutil.WriteFile(".github/workflows/acceptance_test.yml", []byte(s+sData), 0644)
@@ -110,7 +112,7 @@ func printDataSourceOutcome(sortedglobalKeys []string, sortedKeys []string, keyA
 			s = s + fmt.Sprintf(`
   %s:
     needs: %s
-    name: %s
+    name: Test %s Data Source
     runs-on: ubuntu-latest
     steps:
       - name: Checkout
@@ -124,6 +126,55 @@ func printDataSourceOutcome(sortedglobalKeys []string, sortedKeys []string, keyA
         run: |
           echo "ACC_TEST_PROJECT_ID=${{needs.createproject.outputs.projectID}}" >> $GITHUB_ENV
       - name: Test %s Data Source
+        shell: bash
+        run: |
+          make dummy PATH=%s
+`, id, needsstr, name, name, keyAndPathMap[name],
+			)
+			needs[key] = append(needs[key], id)
+		}
+	}
+	return s, strings.Join(nextNeeds, ",")
+
+}
+
+func printResourceOutcome(sortedglobalKeys []string, sortedKeys []string, keyAndPathMap map[string]string, prefix, resNeeds string) (string, string) {
+	s := ""
+	needs := map[string][]string{}
+	nextNeeds := []string{}
+	for _, key := range sortedglobalKeys {
+		if _, ok := needs[key]; !ok {
+			needs[key] = []string{}
+		}
+		for _, name := range sortedKeys {
+			if !strings.HasPrefix(name, key) {
+				continue
+			}
+			id := prefix + strings.ReplaceAll(name, " ", "-")
+			needsstr := strings.Join(needs[key], ",")
+			if len(needsstr) > 0 {
+				needsstr = "[" + resNeeds + "," + needsstr + "]"
+			} else {
+				needsstr = "[" + resNeeds + "]"
+			}
+			nextNeeds = append(nextNeeds, id)
+			s = s + fmt.Sprintf(`
+  %s:
+    needs: %s
+    name: Test %s Resource
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v3
+      - name: Set up Go
+        uses: actions/setup-go@v3
+        with:
+          go-version: 1.18
+      - name: Prepare environment
+        shell: bash
+        run: |
+          echo "ACC_TEST_PROJECT_ID=${{needs.createproject.outputs.projectID}}" >> $GITHUB_ENV
+      - name: Test %s Resource
         shell: bash
         run: |
           make dummy PATH=%s
