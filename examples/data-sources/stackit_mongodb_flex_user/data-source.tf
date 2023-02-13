7d832524018ecc48c5840cb85250e2e7846bbeed
Dean Oren <deangili.oren@mail.schwarz>
2023-02-13 21:39:49 +0100

    add space

--- examples/data-sources/stackit_mongodb_flex_user/data-source.tf
+++ examples/data-sources/stackit_mongodb_flex_user/data-source.tf
@@ -3,6 +3,7 @@ resource "stackit_mongodb_flex_instance" "example" {
   name         = "example"
   machine_type = "1.1"
 }
+
 resource "stackit_mongodb_flex_user" "example" {
   project_id  = var.project_id
   instance_id = stackit_mongodb_flex_instance.example.id
