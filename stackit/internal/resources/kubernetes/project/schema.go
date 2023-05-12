a869b1f8316da7fc9f9eabb4a86c4388ff8c479d
Dean Oren <deangili.oren@mail.schwarz>
2023-05-12 17:02:10 +0200

    validate project ID is UUID

--- stackit/internal/resources/kubernetes/project/schema.go
+++ stackit/internal/resources/kubernetes/project/schema.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 
 	"github.com/SchwarzIT/terraform-provider-stackit/stackit/internal/common"
+	"github.com/SchwarzIT/terraform-provider-stackit/stackit/pkg/validate"
 	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
@@ -38,8 +40,11 @@ func (r *Resource) Schema(ctx context.Context, req resource.SchemaRequest, resp 
 			},
 
 			"project_id": schema.StringAttribute{
-				Description: "the project ID that SKE will be enabled in",
+				Description: "the project UUID that SKE will be enabled in",
 				Required:    true,
+				Validators: []validator.String{
+					validate.ProjectID(),
+				},
 			},
 
 			"timeouts": common.Timeouts(ctx, timeouts.Opts{
