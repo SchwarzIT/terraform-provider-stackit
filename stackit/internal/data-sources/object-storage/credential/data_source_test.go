5ce23710922ea54745ba0ab303b9b99fcce0eee7
Dean Oren <deangili.oren@mail.schwarz>
2023-08-07 16:13:01 +0200

    fix indent

--- stackit/internal/data-sources/object-storage/credential/data_source_test.go
+++ stackit/internal/data-sources/object-storage/credential/data_source_test.go
@@ -57,15 +57,15 @@ func config() string {
 	}
 
 	data "stackit_object_storage_credential" "ex1" {
-		object_storage_project_id 	= stackit_object_storage_project.example.id
-		credentials_group_id 		= data.stackit_object_storage_credentials_group.example.id
-		id		   					= stackit_object_storage_credential.example.id
+		object_storage_project_id	= stackit_object_storage_project.example.id
+		credentials_group_id		= data.stackit_object_storage_credentials_group.example.id
+		id							= stackit_object_storage_credential.example.id
 	}
 
 	data "stackit_object_storage_credential" "ex2" {
-		object_storage_project_id   = stackit_object_storage_project.example.id
-		credentials_group_id 		= data.stackit_object_storage_credentials_group.example.id
-		display_name 				= stackit_object_storage_credential.example.display_name
+		object_storage_project_id	= stackit_object_storage_project.example.id
+		credentials_group_id		= data.stackit_object_storage_credentials_group.example.id
+		display_name				= stackit_object_storage_credential.example.display_name
 	}
 
 	`,
