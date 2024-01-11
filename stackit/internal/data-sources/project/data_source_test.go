83a5f4e279f0e13167660d6b21c52eae3ba801f2
Robert Hoppe <robert.hoppe@mail.schwarz>
2024-01-11 19:01:55 +0100

    Add also key = val

--- stackit/internal/data-sources/project/data_source_test.go
+++ stackit/internal/data-sources/project/data_source_test.go
@@ -49,6 +49,7 @@ func TestAcc_ProjectDataSource(t *testing.T) {
 					resource.TestCheckTypeSetElemAttrPair("stackit_project.example", "container_id", "data.stackit_project.ex1", "container_id"),
 					resource.TestCheckTypeSetElemAttrPair("stackit_project.example", "parent_container_id", "data.stackit_project.ex1", "parent_container_id"),
 					resource.TestCheckTypeSetElemAttrPair("stackit_project.example", "labels.iaas-api", "data.stackit_project.ex1", "labels.iaas-api"),
+					resource.TestCheckTypeSetElemAttrPair("stackit_project.example", "labels.key", "data.stackit_project.ex1", "labels.key"),
 				),
 			},
 		},
@@ -64,6 +65,7 @@ func config(name, owner, billing string) string {
 		parent_container_id = "%s"
 		labels = {
 			"iaas-api" = "true"
+			"key" = "val"
 		}
 	}
 
