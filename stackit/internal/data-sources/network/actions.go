77fc2bd66033d13d91b460d9be2f40916f175481
Emil Georgiev <emil.georgiev@mail.schwarz>
2024-02-22 19:29:51 +0200

    ODJ-3115: add data-source network

---
+++ stackit/internal/data-sources/network/actions.go
@@ -0,0 +1,47 @@
+package network
+
+import (
+	"context"
+	"github.com/SchwarzIT/terraform-provider-stackit/stackit/internal/common"
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Read - lifecycle function
+func (d DataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	c := d.client
+	var n Network
+
+	diags := req.Config.Get(ctx, &n)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	projectID, _ := uuid.Parse(n.ProjectID.String())
+	networkID, _ := uuid.Parse(n.NetworkID.String())
+	res, err := c.IAAS.V1GetNetwork(ctx, projectID, networkID)
+	if agg := common.Validate(&resp.Diagnostics, res, err, "JSON200"); agg != nil {
+		resp.Diagnostics.AddError("failed reading project", agg.Error())
+		return
+	}
+
+	network := res.JSON200
+	prefixes := make([]types.String, len(network.Prefixes))
+	for i, pr := range network.Prefixes {
+		prefixes[i] = types.StringValue(pr)
+	}
+	n.NetworkID = types.StringValue(network.NetworkID.String())
+	n.PublicIp = types.StringValue(*network.PublicIp)
+	n.Prefixes = prefixes
+	n.Name = types.StringValue(network.Name)
+	n.NetworkID = types.StringValue(network.NetworkID.String())
+	n.ProjectID = types.StringValue(projectID.String())
+
+	diags = resp.State.Set(ctx, &n)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
---
+++ stackit/internal/data-sources/network/schema.go
@@ -0,0 +1,82 @@
+package network
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/SchwarzIT/terraform-provider-stackit/stackit/internal/common"
+	"github.com/SchwarzIT/terraform-provider-stackit/stackit/pkg/validate"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Network is the schema model
+type Network struct {
+	Name           types.String   `tfsdk:"name"`
+	NameServers    []types.String `tfsdk:"nameservers"`
+	NetworkID      types.String   `tfsdk:"networkId"`
+	Prefixes       []types.String `tfsdk:"prefixes"`
+	PrefixLengthV4 types.Int64    `tfsdk:"prefixLengthV4"`
+	PublicIp       types.String   `tssdk:"publicIp"`
+	ProjectID      types.String   `tfsdk:"project_id"`
+}
+
+// Schema returns terraform schema structure
+func (r *DataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: fmt.Sprintf("Data source for STACKIT network\n%s",
+			common.EnvironmentInfo(r.urls),
+		),
+		Attributes: map[string]schema.Attribute{
+			"name": schema.StringAttribute{
+				Description: "the name of the network",
+				Required:    true,
+				Validators: []validator.String{
+					validate.NetworkName(),
+				},
+			},
+			"nameservers": schema.ListAttribute{
+				Description: "List of DNS Servers/Nameservers.",
+				Required:    false,
+				ElementType: types.StringType,
+				Validators: []validator.List{
+					validate.NameServers(),
+				},
+			},
+			"networkId": schema.StringAttribute{
+				Description: "The ID of the network",
+				Required:    true,
+			},
+			"prefixes": schema.ListAttribute{
+				Required:    true,
+				ElementType: types.StringType,
+				Validators: []validator.List{
+					validate.Prefixes(),
+				},
+			},
+			"prefixLengthV4": schema.Int64Attribute{
+				Description: "prefix length",
+				Required:    true,
+				Validators: []validator.Int64{
+					validate.PrefixLengthV4(),
+				},
+			},
+			"publicIp": schema.StringAttribute{
+				Description: "public IP address",
+				Computed:    true,
+				Validators: []validator.String{
+					validate.PublicIP(),
+				},
+			},
+			"project_id": schema.StringAttribute{
+				Description: "The project UUID.",
+				Required:    true,
+				Validators: []validator.String{
+					validate.ProjectID(),
+				},
+			},
+		},
+	}
+}
