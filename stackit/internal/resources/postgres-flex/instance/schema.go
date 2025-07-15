c0ecd0bdb7fcb481d5475f6b1e4c16925d8f75f5
Robert Hoppe <robert.hoppe@mail.schwarz>
2025-07-15 16:30:15 +0200

    Do not replace on schema

--- stackit/internal/resources/postgres-flex/instance/schema.go
+++ stackit/internal/resources/postgres-flex/instance/schema.go
@@ -106,9 +106,6 @@ func (r *Resource) Schema(ctx context.Context, req resource.SchemaRequest, resp 
 						Optional:    true,
 						Computed:    true,
 						Default:     stringdefault.StaticString(DefaultStorageClass),
-						PlanModifiers: []planmodifier.String{
-							stringplanmodifier.RequiresReplace(),
-						},
 					},
 					"size": schema.Int64Attribute{
 						Description: "The storage size in GB (min of 5 is required)",
